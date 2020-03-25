3c6ac908159ff05469cd25ede327ca2226a31b99
Alex Taylor <altays633@gmail.com>
2020-03-25 13:57:59 -0400

    adding template literal for markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,78 @@
 function generateMarkdown(data) {
   return `
-# ${data.title}
+  #${data.projectName}
 
-`;
+  <!-- project title should be a variable within a template literal -->
+  
+  [![${data.label}-${data.message}](https://img.shields.io/badge/${data.label}-${data.message}-${data.color})](${data.url})
+  
+  ***
+  
+  ## Description
+  
+  ${data.description}
+  
+  ***
+  
+  ## Table of Contents
+  
+
+  1. [Installation](#Installation)
+  2. [Usage](#Usage)
+  3. [License](#License)
+  4. [Contributing](#Contributing)
+  5. [Tests](#Tests)
+  6. [Questions](#Questions)
+  7. [Acknowledgements](#Acknowledgements)
+  
+  
+  ***
+  
+  ## Installation
+  
+  ${data.installation}
+  
+  ***
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ***
+  
+  ## License
+  
+  Copyright (c) ${data.projectYear} ${data.name}.
+  Released under ${data.license}. See the LICENSE.MD for more details.
+  
+  ***
+  
+  ## Contributing
+  
+ ${data.contributing}
+  
+  ***
+  
+  ## Tests
+  
+  ${data.tests}
+  
+  ***
+  
+  ## Questions
+  
+  For any questions, please contact the owner at ${data.email}
+
+  ${data.avatar}
+  
+  ***
+  
+  ## Acknowledgements
+  
+  ${data.acknowledgement}
+  `
+  
 }
 
 module.exports = generateMarkdown;
+
