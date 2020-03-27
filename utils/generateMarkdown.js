6d9f97b39f0633ef1826ebc5073532a0adcbea68
Alex Taylor <altays633@gmail.com>
2020-03-27 19:40:31 -0400

    updating variables for markdown generation using combinedData object

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,14 @@
-function generateMarkdown(data) {
+function generateMarkdown(combinedData) {
   return `
-  #${data.projectName}
+  # ${combinedData.projectName}
  
-  [![${data.label}-${data.message}](https://img.shields.io/badge/${data.label}-${data.message}-${data.color})](${data.url})
+  [![${combinedData.label}-${combinedData.message}](https://img.shields.io/badge/${combinedData.label}-${combinedData.message}-${combinedData.color})](${combinedData.url})
   
   ***
   
   ## Description
   
-  ${data.description}
+  ${combinedData.description}
   
   ***
   
@@ -28,46 +28,46 @@ function generateMarkdown(data) {
   
   ## Installation
   
-  ${data.installation}
+  ${combinedData.installation}
   
   ***
   
   ## Usage
   
-  ${data.usage}
+  ${combinedData.usage}
   
   ***
   
   ## License
   
-  Copyright (c) ${data.projectYear} ${data.name}.
-  Released under ${data.license}. See the LICENSE.MD for more details.
+  Copyright (c) ${combinedData.projectYear} ${combinedData.name}.
+  Released under ${combinedData.license}. See the LICENSE.MD for more details.
   
   ***
   
   ## Contributing
   
- ${data.contributing}
+  ${combinedData.contributing}
   
   ***
   
   ## Tests
   
-  ${data.tests}
+  ${combinedData.tests}
   
   ***
   
   ## Questions
   
-  For any questions, please contact the owner at ${data.email}
+  For any questions, please contact the owner at ${combinedData.email}
 
-  ${data.avatar}
+  ${combinedData.avatar}
   
   ***
   
   ## Acknowledgements
   
-  ${data.acknowledgement}
+  ${combinedData.acknowledgement}
   `
   
 }
@@ -77,24 +77,24 @@ module.exports = {
   generateMarkdown:generateMarkdown
 }
 
-// testObject = {
-//  projectName: "Test",
+testObject = {
+ projectName: "Test",
 
-//   label: "testing",
-//   message: "Markdown",
-//   color: "green",
-//   url: "https://www.google.com",
-//   description: "testing for markdown",
-//   installation: "something something lorem",
-//   projectYear: "2019",
-//   name: "Alex lorem",
-//   license: "lorem", 
-//   contributing: "don't bother!",
-//   tests: "none!",
-//   email: "nobody@nowhere.com",
-//   avatar:"",
-//   acknowledgement:"nobody"
+  label: "testing",
+  message: "Markdown",
+  color: "green",
+  url: "https://www.google.com",
+  description: "testing for markdown",
+  installation: "something something lorem",
+  projectYear: "2019",
+  name: "Alex lorem",
+  license: "lorem", 
+  contributing: "don't bother!",
+  tests: "none!",
+  email: "nobody@nowhere.com",
+  avatar:"",
+  acknowledgement:"nobody"
   
-// }
+}
 
 // console.log(generateMarkdown(testObject))
