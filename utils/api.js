011bd42850347fd32671a1c64793ed2731edc2df
Alex Taylor <altays633@gmail.com>
2020-03-27 19:25:41 -0400

    modifying api call to return get call and object with email and avatar

--- utils/api.js
+++ utils/api.js
@@ -1,47 +1,32 @@
 const axios = require("axios");
 
-// function getUser(username) {
-//   axios
-//     .get("https://api.github.com/users/"+username)
-//     .then(response => 
-//       // let userImg = response.data.avatarURL;
-//       console.log(response.data.avatar_url + " " + "response.data");
-//       console.log(response))}
-
 function getUser(username) {
-    const queryUrl = `https://api.github.com/users/${username}/events/public`;
+  const queryUrl = `https://api.github.com/users/${username}/events/public`;
 
-    axios.get(queryUrl).then(function(response) {
-      const responseData = response.data;
-      // console.log(responseData)
-      // loop - pull first one with PushEvent
-      let pushEvent;
-      let pushEventEmail;
-      let pushEventAvatar;
-      let pushArray = [];
+    let pushEvent;
+    let pushEventEmail;
+    let pushEventAvatar;
 
-      for (entry of responseData) {
-        if (entry.type === "PushEvent") {
-          pushEvent = entry;
+    return axios.get(queryUrl)
+      .then(function(response){
+
+        const responseData = response.data;
+        for (entry of responseData) {
+          if (entry.type === "PushEvent") {
+            pushEvent = entry;
+          }
         }
-      }
-      
-      pushEventEmail = pushEvent.payload.commits[0].author.email;
-      pushEventAvatar = pushEvent.actor.avatar_url;
-      pushArray = [pushEventEmail, pushEventAvatar];
-      // console.log(pushArray);
-      return pushArray;
+    
+        pushEventEmail = pushEvent.payload.commits[0].author.email;
+        pushEventAvatar = pushEvent.actor.avatar_url;
+        
+        // returning data as an object
+        return {email: pushEventEmail, avatar: pushEventAvatar}
+      }).catch(error => {
+        console.log(error)
+      });
+  };
 
-  });
-
-}
-
-// figure out how to return the push array
-
-// console.log(getUser("altays"));
-
-
-// what am I exporting? review syntax
 module.exports = {
-  getUser:getUser
+  getUser
 }
